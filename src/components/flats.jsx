7cde010f457cdeb648f3f4a241cde5870cbf77f3
matt evans <mattevo10@gmail.com>
2020-12-21 12:19:19 +0000

    inital setup complete

---
+++ src/components/flats.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-lone-blocks
+{ /* <div class="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
+  <div class="card-category">150 EUR</div>
+  <div class="card-description">
+    <h2>Super 60m2 in trendy neighborhood!</h2>
+  </div>
+  <a class="card-link" href="#"></a>
+</div> */ }
